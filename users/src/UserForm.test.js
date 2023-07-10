d43690ad648afff28002923293b21786da36e173
sleeplesseditor <josh@bryson.me.uk>
2023-07-10 23:55:02 +0100

    First test file setup

---
+++ users/src/UserForm.test.js
@@ -0,0 +1,33 @@
+import { render, screen } from '@testing-library/react';
+import user from '@testing-library/user-event';
+import UserForm from './UserForm';
+
+test('it shows two inputs and a button', () => {
+    render(<UserForm />);
+
+    const inputs = screen.getAllByRole('textbox');
+    const button = screen.getByRole('button');
+
+    expect(inputs).toHaveLength(2);
+    expect(button).toBeInTheDocument();
+});
+
+test('it calls onUserAdd when the form is submitted', async () => {
+    const mock = jest.fn();
+
+    render(<UserForm onUserAdd={mock} />);
+
+    const [nameInput, emailInput] = screen.getAllByRole('textbox');
+
+    await user.click(nameInput);
+    await user.keyboard('jane');
+
+    await user.click(emailInput);
+    await user.keyboard('jane@doe.com');
+
+    const button = screen.getByRole('button');
+    await user.click(button);
+
+    expect(mock).toHaveBeenCalled();
+    expect(mock).toHaveBeenCalledWith({ name: 'jane', email: 'jane@doe.com'})
+});
